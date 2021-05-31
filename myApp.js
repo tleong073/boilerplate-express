5bcaa1197b6932219c4b84d12f5d43c87aac5a80
root <root@DESKTOP-KALQFR4.localdomain>
2021-05-31 16:31:12 -0400

    asdasd

--- myApp.js
+++ myApp.js
@@ -35,7 +35,7 @@ app.get('/:word/echo', (req,res) => {
 });
 app.route('/name').get((req,res) => {
     res.json({
-        name: req.query.firstname + ' ' + req.query.lastname
+        name: req.query.first + ' ' + req.query.last
     });
 });
 
